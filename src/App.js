ad2f430501748b02f39bd640bb72b5eaeba8adfe
TheSunshade <82448383+thesunshade@users.noreply.github.com>
2022-09-10 07:53:22 +0530

    update

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import SuttaIndex from "./Components/SuttaIndex.js";
 import Stats from "./Components/Stats.js";
 import LocatorSortedTable from "./Components/LocatorSortedTable.js";
 import { useState } from "react";
+import settingsIcon from "./images/settings.png";
 
 function App() {
   const [destination, setDestination] = useState(localStorage.destination ? localStorage.destination : "SC");
@@ -34,69 +35,87 @@ function App() {
   return (
     <div className="App">
       <div className="settings-bar">
-        <label id="user-input-label" htmlFor="user-input"></label>
-        <input
-          id="user-input"
-          autoFocus
-          type="text"
-          value={filterInput}
-          onChange={event => updateFilterByText(event.target.value)}
-          placeholder="ex. dhamma"
-        />
-        <button
-          onClick={() => {
-            setFilterInput("");
-            setFilterByText("");
-          }}
-        >
-          X
-        </button>
-        <div
-          className="radiobuttonarea"
-          onChange={e => {
-            setDestination(e.target.value);
-            localStorage.destination = e.target.value;
-          }}
-        >
-          <div>
-            <span className="button-title"></span>
-            <label>
-              <input
-                type="radio"
-                name="destination"
-                value="SC"
-                defaultChecked={destination === "SC" ? "checked" : ""}
-              />
-              SC
-            </label>
-            <label>
-              <input
-                type="radio"
-                name="destination"
-                value="CH"
-                defaultChecked={destination === "CH" ? "checked" : ""}
-              />
-              CH
-            </label>
-            <label>
-              <input
-                type="radio"
-                name="destination"
-                value="SCL"
-                defaultChecked={destination === "SCL" ? "checked" : ""}
-              />
-              SCL
-            </label>
+        <div className="top-row">
+          <div className="filter-area">
+            <label id="user-input-label" htmlFor="user-input"></label>
+            <input
+              id="user-input"
+              autoFocus
+              type="text"
+              value={filterInput}
+              onChange={event => updateFilterByText(event.target.value)}
+              placeholder="ex. dhamma"
+            />
+            <button
+              className="clear-filter-button"
+              onClick={() => {
+                setFilterInput("");
+                setFilterByText("");
+              }}
+            >
+              X
+            </button>
           </div>
-        </div>{" "}
-        <button
-          onClick={() => {
-            toggleView();
-          }}
-        >
-          table
-        </button>
-        <button onClick={() => window.scrollTo(0, 0)}>^</button>
+          <button
+            onClick={() => {
+              toggleView();
+            }}
+          >
+            table
+          </button>
+          <button onClick={() => window.scrollTo(0, 0)}>^</button>
+          <div className="settings-button">
+            <img
+              width="15px"
+              src={settingsIcon}
+              alt="Settings Toggle"
+              onClick={() => {
+                document.getElementById("options-area").classList.toggle("hidden");
+              }}
+            ></img>
+          </div>
+        </div>
+        <div id="options-area" className="options-area hidden">
+          <div
+            className="radiobuttonarea"
+            onChange={e => {
+              setDestination(e.target.value);
+              localStorage.destination = e.target.value;
+            }}
+          >
+            <div>
+              <span className="button-title"></span>
+              Open in:
+              <label>
+                <input
+                  type="radio"
+                  name="destination"
+                  value="SC"
+                  defaultChecked={destination === "SC" ? "checked" : ""}
+                />
+                Sutta Central
+              </label>
+              <label>
+                <input
+                  type="radio"
+                  name="destination"
+                  value="CH"
+                  defaultChecked={destination === "CH" ? "checked" : ""}
+                />
+                Citation Helper
+              </label>
+              <label>
+                <input
+                  type="radio"
+                  name="destination"
+                  value="SCL"
+                  defaultChecked={destination === "SCL" ? "checked" : ""}
+                />
+                SC Light
+              </label>
+            </div>
+          </div>
+        </div>
       </div>
       {/* close settings bar */}
       {isLocatorView ? <LocatorSortedTable /> : <SuttaIndex destination={destination} filterByText={filterByText} />}
