dd7ae6a70e9ba940fcbb2f0f41cacc56de04b976
TheSunshade <82448383+thesunshade@users.noreply.github.com>
2022-09-10 13:31:35 +0530

    add docs

--- src/Components/Stats.js
+++ src/Components/Stats.js
@@ -1,5 +1,6 @@
 import { indexData } from "../data/index-data";
 import { indexArray } from "../data/index-array";
+import githubIcon from "../images/GitHub-Mark-64px-black.png";
 
 export default function Stats() {
   const index = JSON.parse(indexArray);
@@ -13,6 +14,14 @@ export default function Stats() {
         <div>
           Number of locators: <span className="number">{index.length}</span>
         </div>
+        <a
+          href="https://github.com/thesunshade/sutta-index#readme"
+          title="The source code for this site"
+          rel="noreferrer"
+          target="_blank"
+        >
+          <img width="20px" src={githubIcon} alt="GitHub icon" />
+        </a>
       </div>
     </>
   );
